511035b3d21ef21bdb667f663e06b35790c93810
Ranjeet Kumar <65223104+ranjeetgupta71@users.noreply.github.com>
2023-09-21 05:38:47 +0530

    Time: 12 ms (5.95%), Space: 43.7 MB (6.20%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,50 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        
+        for(int i=n-1;i>0;i--){
+            int curr = nums[i];
+            int next = nums[i-1];
+    
+            if(next<curr){
+                int minimum = findMin(curr,next,i,nums);
+                System.out.println(minimum+"min");
+                int temp = nums[minimum];
+                nums[minimum] = nums[i-1];
+                nums[i-1] = temp;
+                System.out.println(nums[i]+" : ");
+                sortTheRest(i,nums);
+                return;
+            }
+             
+        }
+        Arrays.sort(nums);
+    }
+    
+    int findMin(int currMin,int compare,int minIndex,int[] nums ){
+        int minimum=minIndex;
+        for(int i=minIndex;i<nums.length;i++){
+            if(nums[i]<currMin && nums[i] > compare){
+                currMin = nums[i];
+                minimum = i;
+            }
+        }
+        // System.out.println(minimum+" : ");
+        return minimum;
+    }
+       
+    void sortTheRest(int curr,int[] nums){
+        for(int i=curr;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                System.out.println(nums[i]+" "+nums[j]);
+                if(nums[i]>nums[j]){
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+    }
+    
+    
+}
