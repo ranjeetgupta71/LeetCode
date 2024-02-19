eaa3a79a3ecf3fe71243f60f370aafd78470d4a8
Ranjeet Kumar <65223104+ranjeetgupta71@users.noreply.github.com>
2024-02-19 18:55:30 +0530

    Time: 32 ms (14.27%), Space: 45 MB (20.87%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,34 +1,22 @@
 class Solution {
     public int maxVowels(String s, int k) {
-        int n = s.length();
-        int count = 0;
         
+        int ans = 0;
+        var vowels = Set.of('a','e','i','o','u');
         
-        for(int i=0;i<k;i++){
+        for(int i=0 , winCount=0; i<s.length();i++){
             char ch = s.charAt(i);
-            if(ch == 'a'|| ch == 'e'||ch == 'i'|| ch == 'o'|| ch == 'u'){
-                    count++;
-            } 
+            if(vowels.contains(ch)){
+                ++winCount;
+            }
+            if(i>=k && vowels.contains(s.charAt(i-k))){
+                --winCount;
+            }
+            ans = Math.max(winCount,ans);
         }
         
-        int j=k-1;
-        int i=0;
-        int ans = count;
-        while(j<n-1){
-            char ch1 = s.charAt(i);
-            char ch2 = s.charAt(j+1);
-            if(ch1 == 'a'|| ch1 == 'e'||ch1 == 'i'|| ch1 == 'o'|| ch1 == 'u'){
-                    count--;      
-            }
-            if(ch2 == 'a'|| ch2 == 'e'||ch2 == 'i'|| ch2 == 'o'|| ch2 == 'u'){
-                    count++;
-            }
-            i++;
-            j++;
-            
-            ans = Math.max(ans,count);
-        }
         return ans;
+        
     }   
 }
 
